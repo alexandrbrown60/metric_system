47842fe361df59e6f2659e3f64380ace37233452
Alexander Ivanov <alexandrbrown60@gmail.com>
2021-12-24 15:54:52 +0300

    Getting general data for objects

--- modules/cron-works/getObjectsFromCrm.php
+++ modules/cron-works/getObjectsFromCrm.php
@@ -7,31 +7,13 @@ require "../classes/crm/CrmManager.php";
 
 $db = new DatabaseManager(DATABASE_NAME);
 $crm = new CrmManager();
+$date = Date("d-m-Y");
 
-//фильтры для всех типов объектов
-$allFlats = [];
-$allHouses = [];
-$allCommercial = [];
+//get all general data and set to db
+$allFlats = $crm->getQuantityFor(1, $flatFields);
+$allCommercial = $crm->getQuantityFor(2, $commercialFields);
+$allHouses = $crm->getQuantityFor(3, $housesFields);
+$generalObjectsData = [$date, $allFlats, $allCommercial, $allHouses];
 
-//фильтры для квартир
-
-//фильтры для коммерции
-
-//фильтры для домов и участков
-
-//Получаем данные из CRM и записываем в БД
-// 1. Общие данные
-$flatsQ = $crm->getQuantityFor(1, $allFlats);
-$housesQ = $crm->getQuantityFor(3, $allHouses);
-$commercialQ = $crm->getQuantityFor(2, $allCommercial);
-
-$sql = "INSERT INTO generalPropertiesData (date, flats, commercial, houses) VALUES (?, ?, ?, ?)";
-$inputs = [date(), $flatsQ, $commercialQ, $housesQ];
-$db->sendRequest($sql, $inputs);
-
-// 2. Данные по квартирам
-
-// 3. Данные по коммерции
-
-// 4. Данные по домам и участкам
-
+$sql = "INSERT INTO generalPropertiesData (date, flats, houses, commercial) VALUES (?, ?, ?, ?)";
+$db->sendRequest($sql, $generalObjectsData);
