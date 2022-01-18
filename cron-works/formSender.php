79c6de8f597aea549cf57bf5d1c53ea4743b984d
Alexander Ivanov <alexandrbrown60@gmail.com>
2022-01-18 11:38:35 +0300

    formSender bug fixed

--- cron-works/formSender.php
+++ cron-works/formSender.php
@@ -3,10 +3,10 @@ header("Content-type: text/html; charset=utf-8");
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-require "../modules/globals.php";
-require "../modules/classes/database/Connection.php";
-require "../modules/classes/database/DatabaseManager.php";
-require "../telegram/classes/Telegram.php";
+require __DIR__."/../modules/globals.php";
+require __DIR__."/../modules/classes/database/Connection.php";
+require __DIR__."/../modules/classes/database/DatabaseManager.php";
+require __DIR__."/../telegram/classes/Telegram.php";
 
 $db = new DatabaseManager(DATABASE_NAME);
 $tg = new Telegram();
